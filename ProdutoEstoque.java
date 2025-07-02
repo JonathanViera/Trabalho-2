7237a1af9fd86a258c254987a39a0d448be3e1f3
JonathanViera <jonathan_anunciacao@id.uff.br>
2025-07-02 20:39:23 -0300

    Update ProdutoEstoque.java

--- ProdutoEstoque.java
+++ ProdutoEstoque.java
@@ -1,5 +1,27 @@
 package modelo;
 
-public class ProdutosEstoque {
+public class ProdutoEstoque {
+    private String nome;
+    private float valor;
+    private int quantidade;
+
+
+    public ProdutoEstoque(String nome, float valor, int quantidade){
+        this.nome = nome;
+        this.valor = valor;
+        this.quantidade = quantidade;
+        
+    }
+    public String getNome() { return nome; }
+    public float getValor(){return valor;}
+    public int getQuantidade(){return quantidade;}
+
+    public void setQuantidade(int quantidade){
+        this.quantidade = quantidade;
+    }
+    @Override
+    public String toString() {
+        return quantidade + " unidades" +  nome + " - R$" + valor;
+    }
 
 }
