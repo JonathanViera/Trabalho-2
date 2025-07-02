36a2336bb911da59918914322e2cdeacadbfa74b
JonathanViera <jonathan_anunciacao@id.uff.br>
2025-07-02 20:39:06 -0300

    Update Item.java

--- Item.java
+++ Item.java
@@ -1,5 +1,18 @@
 package modelo;
 
-public class Itens {
+public class Item {
+    private String nomeProduto;
+    private int quantidade;
+ 
+    public Item(String nomeProduto, int quantidade){
+        this.nomeProduto = nomeProduto;
+        this.quantidade = quantidade;
+    }
 
+    public String getNomeProduto(){
+        return nomeProduto;
+    }
+    public int getQuantidade(){
+        return quantidade;
+    }
 }
