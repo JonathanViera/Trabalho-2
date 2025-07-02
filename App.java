11e2f073c9b730253a8383d4ae55d8f1e64b8732
JonathanViera <jonathan_anunciacao@id.uff.br>
2025-07-02 20:37:48 -0300

    Update App.java

--- App.java
+++ App.java
@@ -1,3 +1,25 @@
-public class Appi {
+import modelo.ProdutoEstoque;
+import modelo.CarrinhoCompra;
+import modelo.EstoqueProdutos;
+import modelo.Item;
 
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        EstoqueProdutos estoque = new EstoqueProdutos();
+            estoque.adicionaProduto(new ProdutoEstoque ("monitor", 500, 100));
+            estoque.adicionaProduto(new ProdutoEstoque ("telefone", 150, 300));
+            estoque.adicionaProduto(new ProdutoEstoque ("teclado", 70, 50));
+            estoque.adicionaProduto(new ProdutoEstoque ("mouse", 50, 50));
+
+            CarrinhoCompra carrinho = new CarrinhoCompra(estoque);
+             carrinho.adicionaItem(new Item("monitor", 2));
+             carrinho.adicionaItem(new Item("telefone", 5));
+             carrinho.adicionaItem(new Item("teclado", 2));
+             
+             carrinho.finalizaCompra();
+             
+             System.out.println("A soma dos produtos : " + carrinho.calculaTotal());
+             
+ }
 }
